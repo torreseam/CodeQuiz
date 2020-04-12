197155f7c6ee61882017218837257c62e77f1872
torreseam <615694479+torreseam@users_noreply.github.com>
2020-04-11 22:30:25 -0500

    update changes to script

--- assets/js/questionscript.js
+++ assets/js/questionscript.js
@@ -41,56 +41,56 @@ if (viewScores.textContent = null) { } else {
     div.innerHTML = JSON.parse(localStorage.getItem('scored'))
     viewScores.appendChild(div);
 }
-var score = 0;
-var submit = document.querySelector("#submit");
-submit.addEventListener("click", function (event) {
-    event.preventDefault();
+// var score = 0;
+// var submit = document.querySelector("#submit");
+// submit.addEventListener("click", function (event) {
+//     event.preventDefault();
 
-    //getting choice of user
-    var checkedRadio = document.querySelector('.radio:checked');
-    var answer;
-    if (checkedRadio === null) {
-        answer = -9;
-    } else {
-        answer = checkedRadio.value;
-    }
-    //console.log(answer);
-    //console.log("check if right");
-    currentIndex++;
-    game();
-    //compare answer and user answer. update score
-    if (answer == questions[currentIndex - 1].answer) {
-        score++;
-    } else { score; }
-    console.log(score);
+//     //getting choice of user
+//     var checkedRadio = document.querySelector('.radio:checked');
+//     var answer;
+//     if (checkedRadio === null) {
+//         answer = -9;
+//     } else {
+//         answer = checkedRadio.value;
+//     }
+//     //console.log(answer);
+//     //console.log("check if right");
+//     currentIndex++;
+//     game();
+//     //compare answer and user answer. update score
+//     if (answer == questions[currentIndex - 1].answer) {
+//         score++;
+//     } else { score; }
+//     console.log(score);
 
-    //clear radio
-    checkedRadio.checked = false;
+//     //clear radio
+//     checkedRadio.checked = false;
 
 
-    //endgame
-    if (currentIndex === 3) {
-        console.log(score);
-        console.log("game over");
-        var scorecard = document.querySelector(".scoreCard");
-        var percentage = document.querySelector("#percentage");
-        scorecard.style.display = "block";
-        gameEl.style.display = "none";
-        var scored = score / 3 * 100
-        var scored = scored.toFixed(2);
-        //console.log(scored + "%");
-        percentage.textContent = scored + "%";
+//     //endgame
+//     if (currentIndex === 3) {
+//         console.log(score);
+//         console.log("game over");
+//         var scorecard = document.querySelector(".scoreCard");
+//         var percentage = document.querySelector("#percentage");
+//         scorecard.style.display = "block";
+//         gameEl.style.display = "none";
+//         var scored = score / 3 * 100
+//         var scored = scored.toFixed(2);
+//         //console.log(scored + "%");
+//         percentage.textContent = scored + "%";
 
-        //store/get/creatediv and append it to scoreboard
-        var viewScores = document.querySelector(".viewHighscores");
-        localStorage.setItem("scored", JSON.stringify(scored));
-        newDiv = document.createElement("div");
-        newDiv.textContent = scored;
-        viewScores.appendChild(newDiv);
-    }
-    
+//         //store/get/creatediv and append it to scoreboard
+//         var viewScores = document.querySelector(".viewHighscores");
+//         localStorage.setItem("scored", JSON.stringify(scored));
+//         newDiv = document.createElement("div");
+//         newDiv.textContent = scored;
+//         viewScores.appendChild(newDiv);
+//     }
+//     //console.log("hi");
 
-});
+// });
 
 // //array of answer choice objects
 // var choices = [
@@ -125,7 +125,7 @@ submit.addEventListener("click", function (event) {
 // var b = document.querySelector("#b");
 // var c = document.querySelector("#c");
 // var d = document.querySelector("#d");
-// ****var gameEl = document.querySelector(".game");
+// var gameEl = document.querySelector(".game");
 // var startBox = document.querySelector(".start");
 
 // //initializing the game
@@ -195,32 +195,32 @@ submit.addEventListener("click", function (event) {
 //         viewScores.style.display = "none";
 //     }
 // });
-function printHighscores() {
-    // either get scores from localstorage or set to empty array
-    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+// function printHighscores() {
+//     // either get scores from localstorage or set to empty array
+//     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    // sort highscores by score property in descending order
-    highscores.sort(function (a, b) {
-        return b.score - a.score;
-    });
+//     // sort highscores by score property in descending order
+//     highscores.sort(function (a, b) {
+//         return b.score - a.score;
+//     });
 
-    highscores.forEach(function (score) {
-        // create li tag for each high score
-        var liTag = document.createElement("li");
-        liTag.textContent = score.initials + " - " + score.score;
+//     highscores.forEach(function (score) {
+//         // create li tag for each high score
+//         var liTag = document.createElement("li");
+//         liTag.textContent = score.initials + " - " + score.score;
 
-        // display on page
-        var olEl = document.getElementById("highscores");
-        olEl.appendChild(liTag);
-    });
-}
+//         // display on page
+//         var olEl = document.getElementById("highscores");
+//         olEl.appendChild(liTag);
+//     });
+// }
 
-function clearHighscores() {
-    window.localStorage.removeItem("highscores");
-    window.location.reload();
-}
+// function clearHighscores() {
+//     window.localStorage.removeItem("highscores");
+//     window.location.reload();
+// }
 
-document.getElementById("clear").onclick = clearHighscores;
+// document.getElementById("clear").onclick = clearHighscores;
 
-// run function when page loads
-printHighscores();
+// // run function when page loads
+// printHighscores();
